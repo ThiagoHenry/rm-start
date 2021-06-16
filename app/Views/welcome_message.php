854130ec73ce0e46bc8f4f938397421731ee134f
ThiagoHenry <thiagohenry1@yahoo.com.br>
2021-06-16 20:35:33 -0300

    start

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link href="<?=base_url("assets/css/style.css")?>" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+	<!-- STYLES -->
+
+</head>
+<body>
+	<div class="container">
+		<h1>Projeto RM</h1>
+	</div>
+</body>
+</html>
